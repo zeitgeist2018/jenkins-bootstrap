95898f84052981398d2a1f656ea9862a3a9e6351
cristian <cristianlobomartin@gmail.com>
2020-04-14 18:46:40 +0200

    Add jobs generator

---
+++ jobs/constants.groovy
@@ -0,0 +1,7 @@
+package constants
+
+class Constants {
+    public static def GIT_CREDENTIALS = 'github'
+    public static def DAYS_TO_KEEP_ORPHAN_BUILDS = 2
+}
+
---
+++ jobs/main.groovy
@@ -0,0 +1,40 @@
+//import static constants.Constants.GIT_CREDENTIALS
+
+def GIT_CREDENTIALS = 'gitlab'
+def BASE_FOLDER = 'test'
+def NUM_TO_KEEP = 2
+
+folder(BASE_FOLDER) {}
+
+JOBS = [
+        "common": [
+                "jenkins-dummy": 'http://192.168.1.128:8082/cristianlm/jenkins-dummy.git'
+        ]
+]
+
+
+JOBS.each{ projectKey, projectJobs ->
+    folder("$BASE_FOLDER/$projectKey") {}
+
+    projectJobs.each{ jobKey, jobRepository ->
+        multibranchPipelineJob("$BASE_FOLDER/$projectKey/$jobKey") {
+            branchSources {
+                git {
+                    id("$BASE_FOLDER.$projectKey.$jobKey")
+                    remote(jobRepository)
+                    #credentialsId GIT_CREDENTIALS
+                    includes('master feature/*')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    daysToKeep(NUM_TO_KEEP)
+                }
+            }
+            triggers {
+                cron('*/30 * * * *')
+            }
+        }
+    }
+}
+
