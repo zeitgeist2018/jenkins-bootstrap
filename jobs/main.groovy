c82f075a084eab7849a15d02945092e3ff1b1d91
cristian <cristianlobomartin@gmail.com>
2020-04-15 22:01:49 +0200

    test

--- jobs/main.groovy
+++ jobs/main.groovy
@@ -2,34 +2,36 @@
 def BASE_FOLDER = 'infrastructure-build'
 def NUM_TO_KEEP = 2
 
+//println build.environment
+println getBinding().getVariables()
+println "GIT_URL ${GIT_URL}"
 folder(BASE_FOLDER) {}
-println env
-JOBS = [
-        "test": [
-                "jenkins-dummy": "${env['GIT_URL']}/infrastructure-build/jenkins-dummy.git"
-        ]
-]
-
-JOBS.each{ projectKey, projectJobs ->
-    folder("$BASE_FOLDER/$projectKey") {}
-
-    projectJobs.each{ jobKey, jobRepository ->
-        multibranchPipelineJob("$BASE_FOLDER/$projectKey/$jobKey") {
-            branchSources {
-                git {
-                    id("$BASE_FOLDER.$projectKey.$jobKey")
-                    remote(jobRepository)
-//                    credentialsId GIT_CREDENTIALS
-                    includes('master feature/*')
-                }
-            }
-            orphanedItemStrategy {
-                discardOldItems {
-                    daysToKeep(NUM_TO_KEEP)
-                }
-            }
-        }
-    }
-}
+//JOBS = [
+//        "test": [
+//                "jenkins-dummy": "infrastructure-build/jenkins-dummy.git"
+//        ]
+//]
+//
+//JOBS.each { projectKey, projectJobs ->
+//    folder("$BASE_FOLDER/$projectKey") {}
+//
+//    projectJobs.each { jobKey, jobRepository ->
+//        multibranchPipelineJob("$BASE_FOLDER/$projectKey/$jobKey") {
+//            branchSources {
+//                git {
+//                    id("$BASE_FOLDER.$projectKey.$jobKey")
+//                    remote("${env['GIT_URL']}/${jobRepository}")
+////                    credentialsId GIT_CREDENTIALS
+//                    includes('master feature/*')
+//                }
+//            }
+//            orphanedItemStrategy {
+//                discardOldItems {
+//                    daysToKeep(NUM_TO_KEEP)
+//                }
+//            }
+//        }
+//    }
+//}
 
 setBuildResult('SUCCESS')
