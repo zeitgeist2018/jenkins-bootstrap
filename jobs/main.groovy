e9709dbe5502c3738570dca8387f5128f1317436
cristian <cristianlobomartin@gmail.com>
2020-04-15 22:16:06 +0200

    Fix git url variable

--- jobs/main.groovy
+++ jobs/main.groovy
@@ -2,36 +2,33 @@
 def BASE_FOLDER = 'infrastructure-build'
 def NUM_TO_KEEP = 2
 
-//println build.environment
-println getBinding().getVariables()
-println "GIT_URL ${GIT_URL}"
 folder(BASE_FOLDER) {}
-//JOBS = [
-//        "test": [
-//                "jenkins-dummy": "infrastructure-build/jenkins-dummy.git"
-//        ]
-//]
-//
-//JOBS.each { projectKey, projectJobs ->
-//    folder("$BASE_FOLDER/$projectKey") {}
-//
-//    projectJobs.each { jobKey, jobRepository ->
-//        multibranchPipelineJob("$BASE_FOLDER/$projectKey/$jobKey") {
-//            branchSources {
-//                git {
-//                    id("$BASE_FOLDER.$projectKey.$jobKey")
-//                    remote("${env['GIT_URL']}/${jobRepository}")
-////                    credentialsId GIT_CREDENTIALS
-//                    includes('master feature/*')
-//                }
-//            }
-//            orphanedItemStrategy {
-//                discardOldItems {
-//                    daysToKeep(NUM_TO_KEEP)
-//                }
-//            }
-//        }
-//    }
-//}
+JOBS = [
+        "test": [
+                "jenkins-dummy": "infrastructure-build/jenkins-dummy.git"
+        ]
+]
 
-setBuildResult('SUCCESS')
+JOBS.each { projectKey, projectJobs ->
+    folder("$BASE_FOLDER/$projectKey") {}
+
+    projectJobs.each { jobKey, jobRepository ->
+        multibranchPipelineJob("$BASE_FOLDER/$projectKey/$jobKey") {
+            branchSources {
+                git {
+                    id("$BASE_FOLDER.$projectKey.$jobKey")
+                    remote("${GIT_URL}/${jobRepository}")
+//                    credentialsId GIT_CREDENTIALS
+                    includes('master feature/*')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    daysToKeep(NUM_TO_KEEP)
+                }
+            }
+        }
+    }
+}
+
+currentBuild.result = 'SUCCESS'
